9638d789e9b0b08b3ae77c0353db072abe36b9d7
Kartikay dev <120315739+kratham001@users.noreply.github.com>
2026-03-21 15:27:57 +0000

    code updated

--- src/utils/jwt.ts
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-export const generateAccessToken = (userId: number) => {
-    return jwt.sign({ id: userId }, process.env.JWT_SECRET as string, { expiresIn: '15m' });
-};
-
-export const generateRefreshToken = (userId: number) => {
-    return jwt.sign({ id: userId }, process.env.JWT_REFRESH_SECRET as string, { expiresIn: '7d' });
-};
